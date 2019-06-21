e116590f4a7445bf30aa02e53eb8c586f6fbf3e5
Chris Psk <chrispsk@gmail.com>
2019-06-21 18:04:03 +0100

    Added reteta update put and patch

--- reteta/tests/test_reteta.py
+++ reteta/tests/test_reteta.py
@@ -150,3 +150,37 @@ class PrivateRetetaApiTests(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredient1, ingredients)
         self.assertIn(ingredient2, ingredients)
+
+    def test_partial_update_reteta(self):
+        """Test updating reteta with patch"""
+        recipe = sample_reteta(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name="spicy")
+        payload = {'title': 'Meat Feast', 'tags': [new_tag.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_reteta(self):
+        """Test updating reteta with put"""
+        recipe = sample_reteta(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        payload = {
+            'title': 'Pepperoni',
+            'time_minutes': 3,
+            'price': 3.00
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
